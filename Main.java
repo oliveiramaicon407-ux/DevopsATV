17809e0995db62a916f7446c722f6ac2d2a10525
oliveiramaicon407-ux <oliveiramaicon407@gmail.com>
2026-08-20 22:23:30 -0300

    Workflow

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá Devops");
         System.out.println("Teste!!!!!");
+        System.out.println("Teste workflow");
     }
 }
