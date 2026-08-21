7cb092cd92fc99a0f568484723a056b78b5cb702
oliveiramaicon407-ux <oliveiramaicon407@gmail.com>
2026-08-20 22:15:49 -0300

    teste!

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá Devops");
-        System.out.println("Pipeline executada com sucesso!");
+        System.out.println("Teste!");
     }
 }
