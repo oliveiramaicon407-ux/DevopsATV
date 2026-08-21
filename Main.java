06b17dc241107d5802321f9f75eec37050f618b7
oliveiramaicon407-ux <oliveiramaicon407@gmail.com>
2026-08-20 22:26:00 -0300

    testando

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá Devops");
-        System.out.println("Teste!!!!!");
+        System.out.println("Teste!!!");
         System.out.println("Teste workflow");
     }
 }
